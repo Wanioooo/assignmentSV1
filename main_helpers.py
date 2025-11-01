1793cce15f9681c260818d1a2a747a7f197f13e3
Wanioooo <syazwaniemuhtad@gmail.com>
2025-11-02 04:47:22 +0800

    Update and rename main_helper.py to main_helpers.py

---
+++ main_helpers.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import streamlit as st
+
+@st.cache_data
+def load_data():
+    """
+    Load the cleaned sleep dataset from the repo.
+    """
+    df = pd.read_csv("new_dataset_academic_performance (1).csv")
+    return df
+
+
+def split_multi_select(df, column_name):
+    """
+    Splits a multi-select column and counts options.
+    """
+    reasons_df = df[column_name].str.get_dummies(sep=';')
+    counts = reasons_df.sum().sort_values(ascending=False)
+    return counts
