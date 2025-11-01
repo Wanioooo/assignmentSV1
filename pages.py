3a72611eeda2eb893779738c1611b9e7e07ab0e0
Wanioooo <syazwaniemuhtad@gmail.com>
2025-11-02 02:56:17 +0800

    Update pages.py

--- pages.py
+++ pages.py
@@ -4,13 +4,13 @@ st.set_page_config(
     page_title="Academic Performance Analysis"
 )
 
-page1 = st.Page('pages/objective1.py', title='Kesan Dalam Pembelajaran', icon=":material/assignment_turned_in:")
-page2 = st.Page('pages/objective2.py', title='Kesan Dalam Pembelajaran', icon=":material/assignment_turned_in:")
+page1 = st.Page('objective1.py', title='Kesan Dalam Pembelajaran', icon=":material/assignment_turned_in:")
+page2 = st.Page('objective2.py', title='Kesan Dalam Pembelajaran', icon=":material/assignment_turned_in:")
 home = st.Page('main.py', title='Homepage', default=True, icon=":material/home:")
 
 pg = st.navigation(
         {
-            "Menu": [page1, page2,home]
+            "Menu": [hone,page1, page2]
         }
     )
 
