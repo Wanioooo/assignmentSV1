da21c6fb9a748d30adf329a801585df86107810e
Wanioooo <syazwaniemuhtad@gmail.com>
2025-11-02 05:14:56 +0800

    Update pages.py

--- pages.py
+++ pages.py
@@ -1,20 +1,19 @@
+# pages.py
+import streamlit as st
+
 st.set_page_config(
     page_title="Academic Performance Analysis",
     layout="wide",
     initial_sidebar_state="expanded"
 )
 
-# Define the pages using the functions created above
+home = st.Page('main.py', title='Homepage', default=True, icon=":material/home:")
 page1 = st.Page('pages/objective1.py', title='Objective 1: Demographics', icon=":material/bar_chart:")
 page2 = st.Page('pages/objective2.py', title='Objective 2: Learning Factors', icon=":material/assignment_turned_in:")
-home = st.Page('main.py', title='Homepage', default=True, icon=":material/home:")
+page3 = st.Page('pages/objective3.py', title='Objective 3: Under Development', icon=":material/lab_profile:")
 
-# Create the navigation menu
-pg = st.navigation(
-    {
-        "Menu": [home, page1, page2] # Corrected the typo (home instead of hone)
-    }
-)
+pg = st.navigation({
+    "Main Menu": [home, page1, page2, page3]
+})
 
-# Run the navigation (This is the final command needed for this approach)
 pg.run()
