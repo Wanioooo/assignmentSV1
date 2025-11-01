6d027bd82f3377d35cd826fd17579bb4c78d15e9
Wanioooo <syazwaniemuhtad@gmail.com>
2025-11-02 05:11:34 +0800

    Update main_helpers.py

--- main_helpers.py
+++ main_helpers.py
@@ -3,17 +3,14 @@ import streamlit as st
 
 @st.cache_data
 def load_data():
-    """
-    Load the cleaned sleep dataset from the repo.
-    """
-    df = pd.read_csv("new_dataset_academic_performance (1).csv")
+    """Load the academic performance dataset from GitHub."""
+    URL = "https://raw.githubusercontent.com/Wanioooo/assignmentSV1/refs/heads/main/new_dataset_academic_performance%20(1).csv"
+    df = pd.read_csv(URL)
     return df
 
 
 def split_multi_select(df, column_name):
-    """
-    Splits a multi-select column and counts options.
-    """
+    """Splits a multi-select column and counts each option frequency."""
     reasons_df = df[column_name].str.get_dummies(sep=';')
     counts = reasons_df.sum().sort_values(ascending=False)
     return counts
